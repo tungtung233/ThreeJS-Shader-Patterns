d173aa5669a53e41b53bdce417002de739014c08
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-28 00:37:28 -0500

    Pattern 3

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -7,6 +7,10 @@ void main()
 
 
   // Pattern 2
-  gl_FragColor = vec4(vUv.x, vUv.y, 0, 1.0);
+  // gl_FragColor = vec4(vUv.x, vUv.y, 0, 1.0);
+
+
+  // Pattern 3
+  gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0);
 
 }
