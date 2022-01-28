bf747a46ad506829869d86806c2c382805d134dc
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-28 13:33:26 -0500

    Pattern 20

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -131,7 +131,15 @@ void main()
 
 
   // Pattern 19
-  float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+  // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+  // gl_FragColor = vec4(strength, strength, strength, 1.0);
+
+
+  // Pattern 20
+  float square1 = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+  float square2 = 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+
+  float strength = square1 * square2; // taking the intersection between the two squares
   gl_FragColor = vec4(strength, strength, strength, 1.0);
 
 }
