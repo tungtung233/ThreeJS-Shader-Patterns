906d3a27b742fc892faf66d33983dd14267b3190
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-28 00:41:31 -0500

    Pattern 11

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -65,8 +65,14 @@ void main()
 
 
   // Pattern 10
-  float strength = mod(vUv.x * 10.0, 1.0);
-  strength = step(0.8, strength);
-  gl_FragColor = vec4(strength, strength, strength, 1.0);
+  // float strength = mod(vUv.x * 10.0, 1.0);
+  // strength = step(0.8, strength);
+  // gl_FragColor = vec4(strength, strength, strength, 1.0);
 
+
+  // Pattern 11
+  float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+  strength += step(0.8, mod(vUv.y * 10.0, 1.0)); 
+  gl_FragColor = vec4(strength, strength, strength, 1.0);
+  
 }
