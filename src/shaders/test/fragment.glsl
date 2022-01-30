f67a27295e1c01bb6fcc98529db9aaeca040e46b
Veronica Leung <tungtungleung233@hotmail.com>
2022-01-30 14:13:14 -0500

    Pattern 39

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -302,12 +302,22 @@ void main()
 
 
   // Pattern 38
+  // vec2 wavedUv = vec2(
+  //   vUv.x + sin(vUv.y * 30.0) * 0.1,
+  //   vUv.y + sin(vUv.x * 30.0) * 0.1
+  // );
+  
+  // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
+  // gl_FragColor = vec4(strength, strength, strength, 1.0);
+
+
+  // Pattern 39
   vec2 wavedUv = vec2(
-    vUv.x + sin(vUv.y * 30.0) * 0.1,
-    vUv.y + sin(vUv.x * 30.0) * 0.1
+    vUv.x + sin(vUv.y * 100.0) * 0.1,
+    vUv.y + sin(vUv.x * 100.0) * 0.1
   );
   
   float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
   gl_FragColor = vec4(strength, strength, strength, 1.0);
-
+  
 }
