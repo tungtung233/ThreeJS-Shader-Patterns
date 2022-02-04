4c3657f6aa8e6bc59545f341db832b4642c2baec
Veronica Leung <tungtungleung233@hotmail.com>
2022-02-04 16:08:06 -0500

    Pattern 42

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -329,8 +329,16 @@ void main()
   
 
   // Pattern 41
+  // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+  // float strength = angle;
+  // gl_FragColor = vec4(strength, strength, strength, 1.0);
+  
+
+  // Pattern 42
   float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+  angle /= PI * 2.0;
+  angle +=  0.5;
   float strength = angle;
   gl_FragColor = vec4(strength, strength, strength, 1.0);
-  
+
 }
